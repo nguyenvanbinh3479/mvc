9477905a2b2653ec460f9e390c22b1aed904b761
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-12 17:22:36 +0700

    create albums_view

---
+++ admin/view/albums/create.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('PATH_PUBLIC')) die ('Bad requested!');
+    require_once(PATH_PUBLIC . '/template/admin/header.php');
+?>
+<div id="wrapper">
+	<div class="container">
+	    <form method="post" action="admin.php">
+	    <input type="hidden" name="c" value="album">
+	    <input type="hidden" name="a" value="store">
+	    <div class="row">   		
+	    	<h2>Create album</h2>
+	    </div>
+	    <div class="row">   		
+	    	<label>Bai Hat:</label>
+	    </div>
+	    <div class="row">
+	    	<select class="form-control p-2 m-2" name="bai_hat_id">
+	    		<option value="admin">Admin</option>
+	    		<option value="user">User</option>
+	    	</select>
+	    </div>
+	    <div class="row">   		
+	    	<label>anh:</label>
+	    </div>
+	    <div class="row">   	
+	    	<input type="text" class="form-control p-2 m-2" name="anh" required>
+	    </div>
+	    <div class="row">   		
+	    	<label>ten:</label>
+	    </div>
+	    <div class="row">   	
+	    	<input type="text" class="form-control p-2 m-2" name="ten" required>
+	    </div>
+	    <div class="row">   
+	    	<button class="btn btn-primary p-2 m-2" type="submit">Apply</button>
+	    </div>
+	    </form>
+	</div>
+</div>
+<?php require_once(PATH_PUBLIC . '/template/admin/footer.php'); ?>
