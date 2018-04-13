3b96bd64c2c56b506d43c10c9f81bf0d2b2e407f
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-13 14:27:32 +0700

    modify chitietplaylist_view

--- admin/view/chitietplaylists/index.php
+++ admin/view/chitietplaylists/index.php
@@ -3,7 +3,7 @@
 ?>
 <div class="container">
     <div class="row">
-        <h3>Danh sach playlist</h3>
+        <h3>Danh sach chitietplaylist</h3>
     </div>
     <table class="table">
         <thead>
@@ -16,13 +16,13 @@
           </tr>
         </thead>
         <tbody>
-        <?php foreach ($list_playlist as $playlist) { ?>    
+        <?php foreach ($list_chitietplaylist as $chitietplaylist) { ?>    
           <tr>
             <td><?php echo increment_once($index); ?></td>
-            <td><?php echo $playlist->playlist_id; ?></td>
-            <td><?php echo $playlist->baihat_id; ?></td>
-            <td><a href="admin.php?c=playlist&a=edit&id=<?php echo $playlist->playlist_id; ?>">Edit</a></td>
-            <td><a href="admin.php?c=playlist&a=delete&id=<?php echo $playlist->playlist_id; ?>">Delete</a></td>
+            <td><?php echo $chitietplaylist->playlist_id; ?></td>
+            <td><?php echo $chitietplaylist->baihat_id; ?></td>
+            <td><a href="admin.php?c=chitietplaylist&a=edit&id=<?php echo $chitietplaylist->playlist_id; ?>">Edit</a></td>
+            <td><a href="admin.php?c=chitietplaylist&a=delete&id=<?php echo $chitietplaylist->playlist_id; ?>">Delete</a></td>
           </tr>
         <?php } ?>
         </tbody>
