9e61467a3beadc7a36e00aad5079697cbc015aa4
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-13 20:12:52 +0700

    modify styles.css

---
+++ login1.php
@@ -0,0 +1,34 @@
+<?php include "index.php"?>
+<?php session_start()?>
+<?php
+    
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        $email = addslashes($_POST['email']);
+        $password = addslashes($_POST['password']);
+        //kiem tra co ton tai khong
+        $query = mysqli_query($conn,"SELECT email, password, role, status FROM users WHERE email='$email'");
+         //Kiểm tra email có đúng định dạng hay không
+        if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $email)){
+            echo "Email này không hợp lệ. Vui long nhập email khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            exit;
+        }
+
+        if(mysqli_num_rows($query) == 0){
+            echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            exit;
+        }
+        //lay mat khau trong database
+        $row = mysqli_fetch_array($query);
+        //so sanh mat khau
+        if($password != $row['password']){
+            echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            exit;
+        }
+
+        if($row['role'] != 'admin' && $row['status'] != 'visible'){
+            echo "Bạn không có quyền truy cập. Vui lòng thử lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            exit;
+        }
+    }
+    
+?>  
