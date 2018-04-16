d528c164e7c7c1162d1b445c5a74c3128079784f
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-16 11:17:12 +0700

    modify baihat_view

--- admin/view/baihats/create.php
+++ admin/view/baihats/create.php
@@ -14,8 +14,8 @@
 	    </div>
 		<div class="row">
 	    	<select class="form-control p-2 m-2" name="casi_id">
-	    		 <?php foreach ( $list_casi as $key => $value) { $arr = (array) $value;?>
-
+	    		 <?php foreach ( $list_casi as $key => $value) { 
+					 $arr = (array) $value;?>
  			<option value="<?php print_r($arr['id']); ?> "> <?php print_r($arr['ten']) ; }?></option>
 	    	</select>
 	    </div>
@@ -43,25 +43,25 @@
 	    	<label>Ten Bai Hat:</label>
 	    </div>
 	    <div class="row">
-				<input type="text" class="form-control p-2 m-2" name="ten">
+				<input type="text" class="form-control p-2 m-2" name="ten" required>
 	    </div>
 		<div class="row">   		
 	    	<label>Anh:</label>
 	    </div>
 	    <div class="row">
-				<input type="text" class="form-control p-2 m-2" name="anh">
+				<input type="text" class="form-control p-2 m-2" name="anh" required>
 	    </div>
 		<div class="row">   		
 	    	<label>Loi Bai Hat:</label>
 	    </div>
 	    <div class="row">
-				<input type="text" class="form-control p-2 m-2" name="loi_bai_hat">
+				<input type="text" class="form-control p-2 m-2" name="loi_bai_hat" required>
 	    </div>
 		<div class="row">   		
 	    	<label>Link:</label>
 	    </div>
 	    <div class="row">
-				<input type="text" class="form-control p-2 m-2" name="link">
+				<input type="text" class="form-control p-2 m-2" name="link" required>
 	    </div>
 	    <div class="row">   
 	    	<button class="btn btn-primary p-2 m-2" type="submit">Apply</button>
