1b0c6e7ee6e90e2dbb1d3f44925b940e007d09fe
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-19 17:18:00 +0700

    modify templates

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="publics/img/msc-icon.png" />
-    <link rel="icon" type="image/png" href="publics/img/msc-icon.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="public/img/msc-icon.png" />
+    <link rel="icon" type="image/png" href="public/img/msc-icon.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>M.S.C - Login</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
