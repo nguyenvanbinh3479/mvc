50a4b9288a4ed61bed60e9ab44854414d1e52345
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-22 11:14:57 +0700

    modify luotnghe_model

--- admin/view/luotnghes/create.php
+++ admin/view/luotnghes/create.php
@@ -130,7 +130,7 @@
 							<input type="text" class="form-control p-2 m-2" name="ngay" placeholder="2018-05-15" required>
 						</div>
 						<div class="row">   
-                            <button class="btn btn-warning p-2 m-2" onclick="demo.showNotification('top','left')">Apply</button>
+                            <button class="btn btn-warning p-2 m-2"  onclick="ntf.showNotification('top','left')">Apply</button>
 							<button class="btn btn-warning p-2 m-2" type="submit" style="padding: 12px 0"><a href="admin.php?c=luotnghe" style="color: white; padding: 14px 30px;">Cancel</a></button>                          
 						</div>
                     </form>
