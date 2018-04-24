5c4e6b23302a2f7f523440851de6469fb5d351b0
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-04-24 16:18:25 +0700

    finish luotnghe

--- admin/view/luotnghes/edit.php
+++ admin/view/luotnghes/edit.php
@@ -157,7 +157,7 @@
 							<input type="text" class="form-control p-2 m-2" name="ngay" value="<?php echo $luotnghe->ngay; ?>">
 						</div>
 						<div class="row">   
-                        <button class="btn btn-warning p-2 m-2" type="submit">Apply</button>                          
+                        <button class="btn btn-warning p-2 m-2" type="submit" onclick="alert('Add success !')">Apply</button>                          
                         <button class="btn btn-warning p-2 m-2" type="submit" style="padding: 12px 0"><a href="admin.php?c=luotnghe" style="color: white; padding: 14px 30px;">Cancel</a></button>                          
 					</div>
                     </form>
