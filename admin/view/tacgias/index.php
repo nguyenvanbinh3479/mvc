48cd71be1777868ee931038a4275a987fd62496d
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-05-07 16:09:50 +0700

    modify tacgia_view

--- admin/view/tacgias/index.php
+++ admin/view/tacgias/index.php
@@ -114,8 +114,8 @@
                                         <tr>
                                           <td><?php echo increment_once($index); ?></td>
                                           <td><?php echo $tacgia->ten; ?></td>
-                                          <td><img src="<?php echo $tacgia->anh; ?>" style="width: 50px; height: 50px;"></img></td>                                                                                      
-                                          <td><?php echo $tacgia->thongtin; ?></td>
+                                          <td><img src="<?php echo $tacgia->anh; ?>" style="width: 50px; height: 50px;"></img></td>
+                                          <td><textarea class="form-control" rows="3" cols="30" disabled> <?php echo $tacgia->thongtin; ?></textarea></td>                                          
                                           <td><a href="admin.php?c=tacgia&a=edit&id=<?php echo $tacgia->id; ?>">Edit</a></td>
                                           <td><a href="admin.php?c=tacgia&a=delete&id=<?php echo $tacgia->id; ?>">Delete</a></td>
                                         </tr>
